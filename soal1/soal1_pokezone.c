13c80e347824e4b271405366aa9e679a3074ba30
IktaS <imamrafiia@gmail.com>
2020-04-02 12:56:28 +0700

    done pokedex

--- soal1/soal1_pokezone.c
+++ soal1/soal1_pokezone.c
@@ -6,79 +6,118 @@
 #include <unistd.h>
 #include <time.h>
 #include <pthread.h>
-#define POKEZONE_PORT 8080
-#define SHOP_PORT 8090
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define POKEMON_SHM    "/tmp/pokemon1"
+#define SHOP_SHM       "/tmp/shop1"
 
 typedef struct shop{
+    pthread_mutex_t lock;
+    pthread_mutexattr_t shrlock;
     int lull_pow;
     int pokeball;
     int berry;
 }shop_stock;
 
-pthread_mutex_t lock1,lock2;
+typedef struct poke{
+    pthread_mutex_t lock;
+    pthread_mutexattr_t shrlock;
 
-void request_parser(char * buffer, char *request[]){
-    char * token = strtok(buffer," | ");
-    strcpy(request[0],token);
-    token = strtok(NULL," | ");
-    strcpy(request[1],token);
-}
+    int escape;
+    int rate;
+    int capture;
+    int pokedollar;
+    int ap;
+    char name[2048];
+}pokemon;
 
-void get_pokemon(char * buffer){
+
+
+void get_pokemon(pokemon * buffer){
+    memset(buffer->name,'\0',sizeof(buffer->name));
+
 
     char name[5][10][20] = {
         {"Bulbasaur","Charmander","Squirtle","Rattata","Caterpie"},
-        {"Pikachu","Eevee","Jigglypufff","Snorlax","Dragonite"},
+        {"Pikachu","Eevee","Jigglypuff","Snorlax","Dragonite"},
         {"Mew","Mewtwo","Moltres","Zapdos","Articuno"}
     };
     srand(time(NULL));
     int type = rand()%100;
     int shiny = rand()%8000;
+
+
     if(type < 80){
         if(shiny == 0){
             int name_rand = rand()%5;
-            strcat(buffer,"10 20 50 5080 ");
-            strcat(buffer,"shiny_");
-            strcat(buffer,name[0][name_rand]);
+            buffer->escape = 10;
+            buffer->rate = 20;
+            buffer->capture = 50;
+            buffer->pokedollar = 5080;
+            buffer->ap = 100;
+            strcat(buffer->name,"shiny ");
+            strcat(buffer->name,name[0][name_rand]);
         }else{
             int name_rand = rand()%5;
-            strcat(buffer,"5 20 70 80 ");
-            strcat(buffer,name[0][name_rand]);
+            buffer->escape = 5;
+            buffer->rate = 20;
+            buffer->capture = 70;
+            buffer->pokedollar = 80;
+            buffer->ap = 100;
+            strcat(buffer->name,name[0][name_rand]);
         }
     }
     if(type < 95 && type >= 80){
         if(shiny == 0){
             int name_rand = rand()%5;
-            strcat(buffer,"15 20 30 5100 ");
-            strcat(buffer,"shiny_");
-            strcat(buffer,name[1][name_rand]);
+            buffer->escape = 15;
+            buffer->rate = 20;
+            buffer->capture = 30;
+            buffer->pokedollar = 5100;
+            buffer->ap = 100;
+            strcat(buffer->name,"shiny ");
+            strcat(buffer->name,name[1][name_rand]);
         }else{
             int name_rand = rand()%5;
-            strcat(buffer,"10 20 50 100 ");
-            strcat(buffer,name[1][name_rand]);
+            buffer->escape = 10;
+            buffer->rate = 20;
+            buffer->capture = 50;
+            buffer->pokedollar = 100;
+            buffer->ap = 100;
+            strcat(buffer->name,name[1][name_rand]);
         }
     }
     if(type < 100 && type >=95){
         if(shiny == 0){
             int name_rand = rand()%5;
-            strcat(buffer,"25 20 10 5200 ");
-            strcat(buffer,"shiny_");
-            strcat(buffer,name[2][name_rand]);
+            buffer->escape = 25;
+            buffer->rate = 20;
+            buffer->capture = 10;
+            buffer->pokedollar = 5200;
+            buffer->ap = 100;
+            strcat(buffer->name,"shiny ");
+            strcat(buffer->name,name[2][name_rand]);
         }else{
             int name_rand = rand()%5;
-            strcat(buffer,"5 20 30 200 ");
-            strcat(buffer,name[2][name_rand]);
+            buffer->escape = 5;
+            buffer->rate = 20;
+            buffer->capture = 30;
+            buffer->pokedollar = 200;
+            buffer->ap = 100;
+            strcat(buffer->name,name[2][name_rand]);
         }
     }
 }
 
 void* pokemon_api(void* args){
-    char * buffer = (char *) args;
+    pokemon * poke = (pokemon *) args;
     while(1){
-        pthread_mutex_lock(&lock1);
-        memset(buffer,0,sizeof(buffer));
-        get_pokemon(buffer);
-        pthread_mutex_unlock(&lock1);
+        pthread_mutex_lock(&poke->lock);
+        get_pokemon(poke);
+        pthread_mutex_unlock(&poke->lock);
         sleep(1);
     }
 }
@@ -87,7 +126,7 @@ void* shop_api(void* args){
     shop_stock * stock = (shop_stock*) args;
     while(1){
         sleep(10);
-        pthread_mutex_lock(&lock2);
+        pthread_mutex_lock(&stock->lock);
         if(stock->lull_pow <190){
             stock->lull_pow += 10;
         }else{
@@ -103,26 +142,25 @@ void* shop_api(void* args){
         }else{
             stock->berry += 200 - stock->berry;
         }
-        pthread_mutex_unlock(&lock2);
+        pthread_mutex_unlock(&stock->lock);
     }
 }
 
 void* runserver(void* args){
     pthread_t poke_thread, shop_thread;
 
-    if (pthread_mutex_init(&lock1, NULL) != 0) {
-        printf("\n mutex init has failed\n");
-        // return 1;
-        exit(EXIT_FAILURE);
-    }
+    int * state = (int *) args;
 
-    if (pthread_mutex_init(&lock2, NULL) != 0) {
-        printf("\n mutex init has failed\n");
-        // return 1;
-        exit(EXIT_FAILURE);
-    }
+    key_t key1 = 1234;
+    pokemon * cur_pokemon;
+    int shmid = shmget(key1,sizeof(pokemon),IPC_CREAT | 0666);
+    cur_pokemon = (pokemon * )shmat(shmid,NULL,0);
 
-    char cur_pokemon[2048];
+
+    pthread_mutexattr_init(&cur_pokemon->shrlock);
+    pthread_mutexattr_setpshared(&cur_pokemon->shrlock,PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&cur_pokemon->lock,&cur_pokemon->shrlock);
+
     int iret = pthread_create(&poke_thread, NULL, pokemon_api, (void*) cur_pokemon);
     if(iret) //jika eror
     {
@@ -130,7 +168,17 @@ void* runserver(void* args){
         exit(EXIT_FAILURE);
     }
 
-    shop_stock * stock = (shop_stock *)malloc(sizeof(shop_stock));
+
+    key_t key2 = 5678;
+    shop_stock * stock;
+    int shmid1 = shmget(key2,sizeof(shop_stock),IPC_CREAT | 0664);
+    stock = (shop_stock * )shmat(shmid1,NULL,0);
+
+
+    pthread_mutexattr_init(&stock->shrlock);
+    pthread_mutexattr_setpshared(&stock->shrlock,PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&stock->lock,&stock->shrlock);
+
     stock->lull_pow = 100;
     stock->pokeball = 100;
     stock->berry = 100;
@@ -141,93 +189,15 @@ void* runserver(void* args){
         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
         exit(EXIT_FAILURE);
     }
-
-
-
-
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(POKEZONE_PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-
-    char buffer[2048] = {0};
-    char * success = "success";
-    char * failed = "failed";
-
     while(1){
-        if (listen(server_fd, 3) < 0) {
-            perror("listen");
-            exit(EXIT_FAILURE);
-        }
-
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-
-        int valread = read(new_socket,buffer,2048);
-
-        char request[2][512];
-        char * token = strtok(buffer," | ");
-        strcpy(request[0],token);
-        token = strtok(NULL," | ");
-        strcpy(request[1],token);
-
-        if(strcmp(request[0], "poke") == 0){
-            send(new_socket,cur_pokemon,strlen(cur_pokemon),0);
-        }else if(strcmp(request[0],"shop")==0){
-            if(strcmp(request[1],"lull_pow")==0){
-                if(stock->lull_pow > 0){
-                    pthread_mutex_lock(&lock2);
-                    stock->lull_pow -= 1;
-                    pthread_mutex_unlock(&lock2);
-                    send(new_socket,success,strlen(success),0);
-                }else{
-                    send(new_socket,failed,strlen(failed),0);
-                }
-            }
-            if(strcmp(request[1],"pokeball")==0){
-                if(stock->pokeball > 0){
-                    pthread_mutex_lock(&lock2);
-                    stock->pokeball -= 1;
-                    pthread_mutex_unlock(&lock2);
-                    send(new_socket,success,strlen(success),0);
-                }else{
-                    send(new_socket,failed,strlen(failed),0);
-                }
-            }
-            if(strcmp(request[1],"berry")==0){
-                if(stock->berry > 0){
-                    pthread_mutex_lock(&lock2);
-                    stock->berry -= 1;
-                    pthread_mutex_unlock(&lock2);
-                    send(new_socket,success,strlen(success),0);
-                }else{
-                    send(new_socket,failed,strlen(failed),0);
-                }
-            }
+        if(*state == 1){
+            shmdt(cur_pokemon);
+            shmctl(shmid,IPC_RMID,NULL);
+            shmdt(stock);
+            shmctl(shmid1,IPC_RMID,NULL);
         }
     }
+
 }
 
 void forkAndKillAll(){
@@ -250,7 +220,9 @@ void forkAndKillAll(){
 int main(int argc, char const *argv[]) {
     pthread_t server_thread;
 
-    int iret = pthread_create(&server_thread,NULL,runserver,NULL);
+    int* state;
+    *state = 0;
+    int iret = pthread_create(&server_thread,NULL,runserver,(void*)state);
     if(iret) //jika eror
     {
         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
@@ -261,5 +233,6 @@ int main(int argc, char const *argv[]) {
     printf("Server is running ...\n");
     printf("Press any button to shutdown\n");
     scanf("%c",&input);
+    *state = 0;
     forkAndKillAll();
 }
--- soal1/soal1_traizone.c
+++ soal1/soal1_traizone.c
@@ -4,45 +4,509 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#define POKEZONE_PORT 8080
-#define SHOP_PORT 8090
+#include <time.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define POKEMON_SHM    "/tmp/pokemon1"
+#define SHOP_SHM       "/tmp/shop1"
 
-int connect_socket(uint16_t _PORT){
-    struct sockaddr_in address;
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
+typedef struct shop{
+    pthread_mutex_t lock;
+    pthread_mutexattr_t shrlock;
+    int lull_pow;
+    int pokeball;
+    int berry;
+}shop_stock;
+
+typedef struct poke{
+    pthread_mutex_t lock;
+    pthread_mutexattr_t shrlock;
+
+    int escape;
+    int rate;
+    int capture;
+    int pokedollar;
+    int ap;
+    char name[2048];
+}pokemon;
+
+typedef struct scene{
+    pthread_mutex_t state_lock;
+    int * state_menu;
+    int * state_cari;
+
+    shop_stock * stock;
+    pokemon * available_pokemon;
+
+    shop_stock * player_stock;
+    pokemon * capturedmode_pokemon;
+
+    int pokedollar;
+    pokemon pokedex[10];
+    int sizepokedex;
+
+    pthread_mutex_t lull_lock;
+    int lullaby_state;
+}pokezone_scene;
+
+typedef struct pokedexpoke{
+    pokezone_scene * curscene;
+    pokemon * pokemon;
+    int index;
+}pokedex_pokemon;
+
+void render_mainmenu(pokezone_scene * curscene){
+    printf("Pilih aksi :\n");
+    switch (*(curscene->state_cari))
+    {
+        case 1:
+            printf("1. Berhenti mencari\n");
+            break;
+        case 0:
+            printf("1. Cari Pokemon\n");
+            break;
     }
-
-    memset(&serv_addr, '0', sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(_PORT);
-
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    return sock;
+    printf("2. Pokedex\n");
+    printf("3. Shop\n");
 }
 
+void render_capturemode(pokezone_scene * curscene){
+    if(curscene->capturedmode_pokemon != NULL)
+        printf("Kamu bertemu dengan %s\n",curscene->capturedmode_pokemon->name);
+    printf("Pilih aksi :\n");
+    printf("1. Tangkap\n");
+    printf("2. Use Lullaby Powder\n");
+    printf("3. To Mainmenu\n");
+    int rando = rand()%100;
+    if(rando < curscene->capturedmode_pokemon->escape && curscene->lullaby_state != 1){
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 0;
+        pthread_mutex_unlock(&(curscene->state_lock));
+        *(curscene->state_cari) = 0;
+        pthread_mutex_lock(&(curscene->lull_lock));
+        curscene->lullaby_state = 0;
+        pthread_mutex_unlock(&(curscene->lull_lock));
+    }
+}
+
+void render_shop(pokezone_scene * curscene){
+    printf("Pokedollar ---- %d\n",curscene->pokedollar);
+    printf("Pilih barang :\n");
+    printf("1. Lullaby Powder  ---- 60\n");
+    printf("2. Pokeball        ---- 5\n");
+    printf("3. Berry           ---- 15\n");
+    printf("4. To Mainmenu\n");
+}
+
+void render_pokedex(pokezone_scene * curscene){
+    for(int i=0; i<curscene->sizepokedex;i++){
+        printf("%s - %d\n",curscene->pokedex[i].name,curscene->pokedex[i].ap);
+    }
+    printf("\n");
+    printf("Pilih aksi :\n");
+    printf("1. Beri Berry\n");
+    printf("2. Lepas Pokemon\n");
+    printf("3. To Mainmenu\n");
+}
+
+void render_lepas(pokezone_scene * curscene){
+    for(int i=0; i<curscene->sizepokedex;i++){
+        printf("%d. %s - %d\n",i+1,curscene->pokedex[i].name,curscene->pokedex[i].pokedollar);
+    }
+    printf("\n");
+    printf("Pilih nomor lepas : ");
+}
+
+void render_goto(pokezone_scene * curscene){
+    printf("Ketemu Pokemon!\n");
+    printf("1. Pergi ke capture mode\n");
+    printf("2. Kembali ke menu sebelumnya\n");
+}
+
+void * cari_pokemon(void* args){
+    pokezone_scene * passed_scene = (pokezone_scene*)args;
+    while(1){
+        if(passed_scene->state_cari == 0) break;
+        int rando = rand()%10;
+        if(rando < 6){
+            memcpy(passed_scene->capturedmode_pokemon,passed_scene->available_pokemon,sizeof(pokemon));
+            // pthread_mutex_lock(&(passed_scene->state_lock));
+            *(passed_scene->state_menu) = 5;
+            // pthread_mutex_unlock(&(passed_scene->state_lock));
+            *(passed_scene->state_cari) = 0;
+            break;
+        }
+        sleep(10);
+    }
+}
+
+void start_cari_pokemon(pokezone_scene * curscene){
+    pthread_t thread_nyari;
+
+    int iret = pthread_create(&thread_nyari,NULL,cari_pokemon,(void*)curscene);
+    if(iret){
+         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+void input_mainmenu(pokezone_scene * curscene, int input){
+    switch (input)
+    {
+    case 1:
+        // printf("1. Berhenti mencari\n");
+        if(*(curscene->state_cari) == 0) {
+            // pthread_mutex_lock(&(curscene->state_lock));
+            *(curscene->state_cari) = 1;
+            // pthread_mutex_unlock(&(curscene->state_lock));
+            start_cari_pokemon(curscene);
+            break;
+        }
+        if(*(curscene->state_cari) == 1){
+            // pthread_mutex_lock(&(curscene->state_lock));
+            *(curscene->state_cari) = 0;
+            // pthread_mutex_unlock(&(curscene->state_lock));
+            break;
+        }
+        break;
+    case 2:
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 2;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        break;
+    case 3:
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 3;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        break;
+    }
+}
+
+void* calculate_escape_rate(void* args){
+    pokezone_scene * curscene = (pokezone_scene *)args;
+    int escaperate = curscene->capturedmode_pokemon->capture;
+    while(1){
+        if(*(curscene->state_menu) != 1) break;
+        curscene->capturedmode_pokemon->capture += escaperate;
+        sleep(curscene->capturedmode_pokemon->rate);
+    }
+}
+
+void remove_pokemon_from_pokedex(pokezone_scene * curscene,int index){
+    for(int i = index; i < curscene->sizepokedex; i++){
+        memcpy(curscene->pokedex + i,curscene->pokedex + i + 1,sizeof(pokemon));
+    }
+    curscene->sizepokedex--;
+}
+void start_capture_mode(pokezone_scene * curscene){
+    pthread_t pokemon_thread;
+
+    int iret = pthread_create(&pokemon_thread,NULL,calculate_escape_rate,(void*)curscene);
+    if(iret){
+        fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+        exit(EXIT_FAILURE);
+    }
+}
+void* calculate_ap_pokemon(void* args){
+    pokedex_pokemon * pokemon = (pokedex_pokemon *)args;
+    while(1){
+        if(*(pokemon->curscene->state_menu) == 1) continue;
+        sleep(10);
+        if(pokemon->pokemon->ap <= 0){
+            int rando = rand()%10;
+            if(rando == 0){
+                pthread_mutex_lock(&(pokemon->pokemon->lock));
+                pokemon->pokemon->ap = 50;
+                pthread_mutex_unlock(&(pokemon->pokemon->lock));
+            }else{
+                remove_pokemon_from_pokedex(pokemon->curscene,pokemon->index);
+            }
+        }
+        pthread_mutex_lock(&(pokemon->pokemon->lock));
+        pokemon->pokemon->ap -= 10;
+        pthread_mutex_unlock(&(pokemon->pokemon->lock));
+    }
+}
+
+void tangkap_action(pokezone_scene * curscene){
+    if(curscene->player_stock->pokeball <= 0) {
+        printf("habis");
+        return;
+    }
+    int rando = rand()%100;
+    if(rando > (curscene->capturedmode_pokemon->capture + 20 * curscene->lullaby_state)){
+        if(curscene->sizepokedex >=7){
+            curscene->pokedollar += curscene->capturedmode_pokemon->pokedollar;
+            return;
+        }
+        printf("ketangkap");
+        pokemon * put_to_pokedex = (pokemon *)malloc(sizeof(pokemon));
+        memcpy(put_to_pokedex,curscene->capturedmode_pokemon, sizeof(pokemon));
+        memcpy(curscene->pokedex + curscene->sizepokedex,put_to_pokedex,sizeof(pokemon));
+
+        pthread_mutex_init(&((curscene->pokedex + curscene->sizepokedex)->lock),NULL);
+
+        pokedex_pokemon * pokedekusu = (pokedex_pokemon*)malloc(sizeof(pokedex_pokemon));
+        pokedekusu->curscene = curscene;
+        pokedekusu->pokemon = curscene->pokedex + curscene->sizepokedex;
+        pokedekusu->index = curscene->sizepokedex;
+
+        pthread_t new_thread;
+
+        int iret = pthread_create(&new_thread,NULL,calculate_ap_pokemon,(void*)pokedekusu);
+        if(iret){
+            fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+            exit(EXIT_FAILURE);
+        }
+
+        curscene->sizepokedex++;
+        curscene->player_stock->pokeball--;
+
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 0;
+        pthread_mutex_unlock(&(curscene->state_lock));
+        *(curscene->state_cari) = 0;
+
+    }
+}
+void *lull_powder(void* args){
+    int init_time = time(NULL);
+    pokezone_scene * curscene = (pokezone_scene*)args;
+
+    while(1){
+        if((init_time - time(NULL)) == 10){
+            pthread_mutex_lock(&(curscene->lull_lock));
+            curscene->lullaby_state = 0;
+            pthread_mutex_unlock(&(curscene->lull_lock));
+        }
+        pthread_mutex_lock(&(curscene->lull_lock));
+        curscene->lullaby_state = 1;
+        pthread_mutex_unlock(&(curscene->lull_lock));
+    }
+}
+void activate_lull(pokezone_scene * curscene){
+    if(curscene->player_stock->lull_pow <= 0)return;
+    pthread_t lull_thread;
+
+    int iret = pthread_create(&lull_thread,NULL,lull_powder,(void*)curscene);
+    if(iret){
+        fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+        exit(EXIT_FAILURE);
+    }
+    curscene->player_stock->lull_pow--;
+}
+void input_capturemode(pokezone_scene * curscene, int input){
+    start_capture_mode(curscene);
+    switch (input)
+    {
+    case 1:
+        tangkap_action(curscene);
+        break;
+    case 2:
+        activate_lull(curscene);
+        break;
+    case 3:
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 0;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        pthread_mutex_lock(&(curscene->lull_lock));
+        curscene->lullaby_state = 0;
+        pthread_mutex_unlock(&(curscene->lull_lock));
+        break;
+    }
+}
+
+void input_shop(pokezone_scene * curscene, int input){
+    switch (input)
+    {
+    case 1:
+        //fungsi beli lullaby
+        break;
+    case 2:
+        //fungsi belu pokeball
+        break;
+    case 3:
+        //fungsi beli berry
+        break;
+    case 4:
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 0;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        break;
+    }
+}
+
+void give_berry(pokezone_scene * curscene){
+    for(int i=0;i<curscene->sizepokedex;i++){
+        pthread_mutex_lock(&(curscene->pokedex[i].lock));
+        curscene->pokedex[i].ap += 10;
+        pthread_mutex_unlock(&(curscene->pokedex[i].lock));
+    }
+}
+
+void input_pokedex(pokezone_scene * curscene, int input){
+    switch (input)
+    {
+    case 1:
+        give_berry(curscene);
+        break;
+    case 2:
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 4;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        break;
+    case 3:
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 0;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        break;
+    }
+}
+
+void input_lepas(pokezone_scene * curscene, int input){
+    printf("%d %d\n",curscene->pokedollar,curscene->pokedex[input-1].pokedollar);
+    curscene->pokedollar += curscene->pokedex[input-1].pokedollar;
+    remove_pokemon_from_pokedex(curscene,input-1);
+
+    // pthread_mutex_lock(&(curscene->state_lock));
+    *(curscene->state_menu) = 2;
+    // pthread_mutex_unlock(&(curscene->state_lock));
+}
+
+void input_goto(pokezone_scene * curscene, int input){
+    // int before = *(curscene->state_menu);
+    switch (input)
+    {
+    case 1:
+        pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 1;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        break;
+    
+    case 2:
+        // pthread_mutex_lock(&(curscene->state_lock));
+        *(curscene->state_menu) = 0;
+        // pthread_mutex_unlock(&(curscene->state_lock));
+        *(curscene->state_cari) = 0;
+        break;
+    }
+}
+
+void* render_scene(void* args){
+    struct timespec ts;
+    int rs;
+
+    ts.tv_sec = 1/30;
+    ts.tv_nsec = 33333333;
+    pokezone_scene * curscene = (pokezone_scene *)args;
+    while(1){
+        system("clear");
+        switch (*(curscene->state_menu)){
+            case 1:
+                render_capturemode(curscene);
+                break;
+            case 2:
+                render_pokedex(curscene);
+                break;
+            case 3:
+                render_shop(curscene);
+                break;
+            case 4:
+                render_lepas(curscene);
+                break;
+            case 5:
+                render_goto(curscene);
+                break;
+
+            default:
+                render_mainmenu(curscene);
+                break;
+        }
+        // usleep(166666);
+        // rs = nanosleep(&ts,&ts);
+        sleep(1);
+    }
+}
+
+
 int main(int argc, char const *argv[]){
-    int sock = connect_socket(POKEZONE_PORT);
+    srand(time(NULL));
+    // system("/bin/stty raw");
+    //state stuff
+    int state_menu = 0;
+    int state_cari = 0;
 
-    char *hello = "shop | lull_pow";
-    send(sock,hello, strlen(hello),0);
 
-    char buffer[2048] = {0};
+    //shared stuff
+    key_t key1 = 1234;
+    pokemon * cur_pokemon;
+    int shmid = shmget(key1,sizeof(pokemon),IPC_CREAT | 0666);
+    cur_pokemon = (pokemon * )shmat(shmid,NULL,0);
 
-    int valread = read(sock,buffer,2048);
-    printf("%s\n",buffer);
-    while(1){}
+    key_t key2 = 5678;
+    shop_stock * stock;
+    int shmid1 = shmget(key2,sizeof(shop_stock),IPC_CREAT | 0664);
+    stock = (shop_stock * )shmat(shmid1,NULL,0);
+
+    pokemon captured_pokemon;
+
+    shop_stock player_stock;
+    player_stock.lull_pow = 10;
+    player_stock.pokeball = 100;
+    player_stock.berry = 0;
+
+    pokezone_scene * curscene = (pokezone_scene *)malloc(sizeof(pokezone_scene));
+    curscene->state_menu = &state_menu;
+    curscene->state_cari = &state_cari;
+    curscene->available_pokemon = cur_pokemon;
+    curscene->capturedmode_pokemon = &captured_pokemon;
+    curscene->stock = stock;
+    curscene->player_stock = &player_stock;
+    curscene->pokedollar = 0;
+    curscene->sizepokedex = 0;
+    curscene->lullaby_state = 0;
+    pthread_mutex_init(&(curscene->lull_lock),NULL);
+    pthread_mutex_init(&(curscene->state_lock),NULL);
+
+    // printf("current pokemon : %s\n",cur_pokemon->name);
+    int input;
+
+    pthread_t render_thread;
+
+    int iret = pthread_create(&render_thread,NULL,render_scene,(void*)curscene);
+    if(iret){
+         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        scanf("%d",&input);
+        switch (state_menu){
+        case 1:
+            input_capturemode(curscene, input);
+            break;
+        case 2:
+            input_pokedex(curscene, input);
+            break;
+        case 3:
+            input_shop(curscene, input);
+            break;
+        case 4:
+            input_lepas(curscene, input);
+            break;
+        case 5:
+            input_goto(curscene,input);
+            break;
+
+        default:
+            input_mainmenu(curscene, input);
+            break;
+        }
+    }
+    // system("/bin/stty raw");
 }
