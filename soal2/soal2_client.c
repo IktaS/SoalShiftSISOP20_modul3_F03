f491e5237651479a31fd029fd4120f1226b72bda
IktaS <imamrafiia@gmail.com>
2020-04-03 00:01:51 +0700

    soal2 selesai dikit

---
+++ soal2/soal2_client.c
@@ -0,0 +1,311 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <termios.h>
+#define PORT 8080
+
+static struct termios old, current;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  current = old; /* make new settings same as old settings */
+  current.c_lflag &= ~ICANON; /* disable buffered i/o */
+  if (echo) {
+      current.c_lflag |= ECHO; /* set echo mode */
+  } else {
+      current.c_lflag &= ~ECHO; /* set no echo mode */
+  }
+  tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+
+
+typedef struct socket_s{
+    int * server_fd, *new_socket;
+    struct sockaddr_in * address;
+    int * opt;
+    int * addrlen;
+}socket_t;
+
+typedef struct scene{
+    int * scene_state;
+    int * health;
+    int * is_alive;
+    int * is_win;
+    int * is_over;
+    socket_t * socket;
+}scene_data;
+
+void sendRequest(int fd, void * buf, size_t size, int flag){
+    char buffer[2048] = {0};
+    memcpy(buffer,buf,size);
+    // printf("sending %s\n",buffer);
+    if(send(fd,buffer,sizeof(buffer),flag)< 0){
+        perror("send error");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int readResponse(int fd,void * buf, size_t size){
+    char buffer[2048] = {0};
+    int val;
+	if((val =read(fd, buffer, sizeof(buffer))) < 0){
+		printf("read error");
+		exit(EXIT_FAILURE);
+	}
+    // printf("reading %s\n",buffer);
+	memcpy(buf, buffer, size);
+    return val;
+}
+
+
+void render_mainmenu(scene_data * curscene){
+    char request[2048];
+    printf("1. Login\n");
+    printf("2. Register\n");
+    printf("   Choices : ");
+    scanf("%s",request);
+    if(strcmp(request,"login")==0){
+        *(curscene->scene_state) = 1;
+    }else if(strcmp(request,"register")==0){
+        *(curscene->scene_state) = 2;
+    }
+}
+
+void render_login(scene_data * curscene){
+    char * request = "login";
+    sendRequest(*(curscene->socket->server_fd),request,strlen(request),0);
+    char userbuf[100];
+    char passbuf[100];
+    char buffer[2048] = {0};
+    printf("Username : ");
+    scanf("%s",userbuf);
+    sendRequest(*(curscene->socket->server_fd),userbuf,strlen(userbuf),0);
+    printf("Password : ");
+    scanf("%s",passbuf);
+    sendRequest(*(curscene->socket->server_fd),passbuf,strlen(passbuf),0);
+    readResponse(*(curscene->socket->server_fd),buffer,sizeof(buffer));
+    if(strcmp(buffer,"login_success")==0){
+        printf("login success\n");
+        *(curscene->scene_state) = 3;
+    }else{
+        printf("login failed\n");
+        *(curscene->scene_state) = 0;
+    }
+}
+
+void render_register(scene_data * curscene){
+    char * request = "register";
+    sendRequest(*(curscene->socket->server_fd),request,strlen(request),0);
+    char userbuf[100];
+    char passbuf[100];
+    char buffer[2048] = {0};
+    printf("Username : ");
+    scanf("%s",userbuf);
+    sendRequest(*(curscene->socket->server_fd),userbuf,strlen(userbuf),0);
+    printf("Password : ");
+    scanf("%s",passbuf);
+    sendRequest(*(curscene->socket->server_fd),passbuf,strlen(passbuf),0);
+    readResponse(*(curscene->socket->server_fd),buffer,sizeof(buffer));
+    if(strcmp(buffer,"register_success")==0){
+        printf("register success\n");
+    }
+    *(curscene->scene_state) = 0;
+}
+
+void render_menu2(scene_data * curscene){
+    char request[2048];
+    printf("1. Find Match\n");
+    printf("2. Logout\n");
+    printf("   Choices : ");
+    scanf("%s",request);
+    if(strcmp(request,"find")==0){
+        *(curscene->scene_state) = 4;
+    }else if(strcmp(request,"logout")==0){
+        sendRequest(*(curscene->socket->server_fd),request,strlen(request),0);
+        *(curscene->scene_state) = 0;
+    }
+}
+
+void render_waiting(scene_data * curscene){
+    char * request = "find";
+    sendRequest(*(curscene->socket->server_fd),request,strlen(request),0);
+    char buffer[2048] = {0};
+    do{
+        readResponse(*(curscene->socket->server_fd),buffer,sizeof(buffer));
+        printf("Waiting for player....\n");
+        sleep(1);
+    }while(strcmp(buffer,"match_found")!=0);
+    *(curscene->scene_state) = 5;
+}
+
+void* reading_thread(void* args){
+    scene_data * curscene = (scene_data *)args;
+    char buffer[2048] = {0};
+    int win;
+    char * lose = "lose";
+    do{
+        int val = readResponse(*(curscene->socket->server_fd),buffer,sizeof(buffer));
+        if(val == 0)break;
+        if(strcmp(buffer,"damage")==0){
+            *(curscene->health) -= 10;
+            if(*(curscene->health) <= 0){
+                sendRequest(*(curscene->socket->server_fd),lose,strlen(lose),0);
+                *(curscene->is_over) = 1;
+                *(curscene->is_win) = 0;
+                break;
+            }
+            continue;
+        }
+        if(strcmp(buffer,"win") == 0){
+            *(curscene->is_over) = 1;
+            *(curscene->is_win) = 1;
+        }
+    }while(*(curscene->is_over) == 0);
+}
+
+void* rendering(void* args){
+    scene_data * curscene = (scene_data *)args;
+    system("clear");
+    while(*(curscene->is_over) == 0){
+        printf("%d health\n",*(curscene->health));
+        system("clear");
+    }
+}
+
+void render_match(scene_data * curscene){
+    *(curscene->health) = 100;
+    *(curscene->is_alive) = 1;
+    *(curscene->is_win) = 0;
+    *(curscene->is_over) = 0;
+
+    pthread_t readingthread;
+
+    int iret = pthread_create(&readingthread,NULL,reading_thread,(void*)curscene);
+    if(iret){
+        perror("thread error");
+        exit(EXIT_FAILURE);
+    }
+    char * shoot = "shoot";
+    printf("Game dimulai silahkan tap tap secepat mungkin !!\n");
+    char input;
+
+    while(*(curscene->is_over) == 0){
+        input = getch();
+        if(input == ' '){
+            printf("%d health\n",*(curscene->health));
+            sendRequest(*(curscene->socket->server_fd),shoot,strlen(shoot),0);
+            printf("hit!!\n");
+        }
+    }
+    pthread_join(readingthread,NULL);
+    if(*(curscene->is_win) == 1){
+        printf("Game berakhir kamu menang\n");
+    }else if(*(curscene->is_win) == 0){
+        printf("Game berakhir kamu kalah\n");
+    }
+    char * endgame = "endgame";
+    sendRequest(*(curscene->socket->server_fd),endgame,strlen(endgame),0);
+    *(curscene->scene_state) = 3;
+}
+
+
+int main(int argc, char const *argv[]) {
+    socket_t * server = (socket_t*)malloc(sizeof(socket_t));
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+
+    server->address = &serv_addr;
+    server->server_fd = &sock;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    int scene_state = 0;
+    int health = 100;
+    int is_alive = 1;
+    int is_win = 0;
+    int is_over = 0;
+    scene_data * curscene = (scene_data *)malloc(sizeof(scene_data));
+    curscene->scene_state = &scene_state;
+    curscene->socket = server;
+    curscene->health = &health;
+    curscene->is_alive = &is_alive;
+    curscene->is_win = &is_win;
+    curscene->is_over = &is_over;
+
+    while(1){
+        switch (scene_state)
+        {
+        case 0:
+            render_mainmenu(curscene);
+            break;
+        case 1:
+            render_login(curscene);
+            break;
+        case 2:
+            render_register(curscene);
+            break;
+        case 3:
+            render_menu2(curscene);
+            break;
+        case 4:
+            render_waiting(curscene);
+            break;
+        case 5:
+            render_match(curscene);
+            break;
+        }
+    }
+}
---
+++ soal2/soal2_server.c
@@ -0,0 +1,360 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define TRUE   1
+#define FALSE  0
+#define FILE_AKUN   "/home/ikta/akun.txt"
+#define PORT 8080
+
+
+typedef struct player_s{
+    int socket_id;
+    int * login;
+    int * in_match;
+
+    struct player_s * enemy;
+}player_t;
+typedef struct node_s{
+    player_t * data;
+    struct node_s * next;
+}node_t;
+
+typedef struct queue_s{
+    struct node_s * back;
+    struct node_s * front;
+    int count;
+}queue_t;
+
+node_t * init_node(player_t * data){
+    node_t * tmp = (node_t *)malloc(sizeof(node_t));
+    tmp->data = data;
+    tmp->next = NULL;
+}
+
+queue_t * init_queue(){
+    queue_t * temp_queue = (queue_t *)malloc(sizeof(queue_t));
+    temp_queue->count = 0;
+    temp_queue->back = NULL;
+    temp_queue->front = NULL;
+    return temp_queue;
+}
+
+
+void enqueue(player_t * player, queue_t * queue){
+    node_t * tmp = init_node(player);
+    if(queue->back == NULL && queue->front == NULL){
+        queue->front = tmp;
+    }
+    else if(queue->back == queue->front){
+        queue->front->next = tmp;
+    }
+    else{
+        queue->back->next = tmp;
+    }
+    queue->back = tmp;
+    queue->count++;
+}
+
+player_t * dequeue(queue_t * queue){
+    if(queue->front == NULL)return NULL;
+    node_t * temp = queue->front;
+    player_t * tmp = queue->front->data;
+    queue->front = queue->front->next;
+    free(temp);
+    queue->count--;
+    return tmp;
+}
+
+
+typedef struct socket_s{
+    int * server_fd, *new_socket;
+    struct sockaddr_in * address;
+    int * opt;
+    int * addrlen;
+}socket_t;
+
+typedef struct server_s{
+    socket_t * server_socket;
+    queue_t * player_queue;
+    FILE * akun;
+}server_t;
+
+typedef struct player_thread{
+    player_t * player;
+    server_t * server;
+}player_args;
+
+int readRequest(int fd,void * buf, size_t size){
+    char buffer[2048];
+    memset(buffer,0,sizeof(buffer));
+    int val;
+	if((val =read(fd, buffer, sizeof(buffer))) < 0){
+		printf("read error");
+		exit(EXIT_FAILURE);
+	}
+    // printf("reading %s\n",buffer);
+	memcpy(buf, buffer, size);
+    return val;
+}
+
+void sendResponse(int fd, void * buf, size_t size, int flag){
+    char buffer[2048];
+    memset(buffer,0,sizeof(buffer));
+    memcpy(buffer,buf,size);
+    // printf("sending %s\n",buffer);
+    if(send(fd,buffer,sizeof(buffer),flag)< 0){
+        perror("send error");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void* match(void* args){
+    server_t * serverMain = (server_t *) args;
+    char * findmessage = "match_found";
+    while(1){
+        if(serverMain->player_queue->count >= 2){
+            // printf("dequque ing\n");
+            player_t * player1 = dequeue(serverMain->player_queue);
+            player_t * player2 = dequeue(serverMain->player_queue);
+            player1->enemy = player2;
+            player2->enemy = player1;
+            sendResponse(player1->socket_id,findmessage,strlen(findmessage),0);
+            sendResponse(player2->socket_id,findmessage,strlen(findmessage),0);
+            *(player1->in_match) = 1;
+            *(player1->in_match) = 1;
+        }
+    }
+}
+
+void add_account(char * username, char * password){
+    FILE * temp = fopen(FILE_AKUN,"a+");
+    char buffer[2048] = {0};
+    strcat(buffer,username);
+    strcat(buffer," | ");
+    strcat(buffer,password);
+    printf("key generated! %s\n",buffer);
+    fprintf(temp,"%s\n",buffer);
+    fclose(temp);
+}
+
+int check_account(char * username, char * password){
+    char user_buf[100];
+    char pass_buf[100];
+    int auth = 0;
+    FILE * temp = fopen(FILE_AKUN,"a+");
+    while(fscanf(temp,"%s | %s",user_buf,pass_buf) != EOF){
+        if(strcmp(username, user_buf) == 0){
+            if(strcmp(password,pass_buf)==0){
+                auth = 1;
+                break;
+            }
+            continue;
+        }
+    }
+    return auth;
+}
+
+void * player_handler(void* args){
+    printf("player handler in\n");
+    player_args * t_args = (player_args *)args;
+    server_t * server = t_args->server;
+    player_t * player = t_args->player;
+
+
+    while(1){
+        char buffer[2048];
+        memset(buffer,0,sizeof(buffer));
+        printf("try reading request\n");
+        int val = read(player->socket_id,buffer,sizeof(buffer));
+        printf("got request!  %s\n",buffer);
+        if(val == 0)break;
+        if(strcmp(buffer,"login")==0){
+
+            char user_[100];
+            char pass_[100];
+
+            printf("reading username...\n");
+            readRequest(player->socket_id,user_,sizeof(user_));
+            printf("got username! %s\n",user_);
+
+            printf("reading password...\n");
+            readRequest(player->socket_id,pass_,sizeof(pass_));
+            printf("got password! %s\n",pass_);
+
+            int auth = check_account(user_,pass_);
+            if(auth){
+                char * message = "login_success";
+                sendResponse(player->socket_id,message,strlen(message),0);
+                *(player->login) = 1;
+                fflush(stdout);
+            }else{
+                char * message = "login_failed";
+                sendResponse(player->socket_id,message,strlen(message),0);
+                *(player->login) = 0;
+                fflush(stdout);
+            }
+        }
+        if(strcmp(buffer,"register")==0){
+            char user_[100];
+            char pass_[100];
+
+
+            printf("reading username...\n");
+            readRequest(player->socket_id,user_,sizeof(user_));
+            printf("got username! %s\n",user_);
+
+
+            printf("reading password...\n");
+            readRequest(player->socket_id,pass_,sizeof(pass_));
+            printf("got password! %s\n",pass_);
+
+
+            add_account(user_,pass_);
+            char * registersuccess = "register_success";
+            sendResponse(player->socket_id,registersuccess,strlen(registersuccess),0);
+        }
+
+        if(strcmp(buffer,"find")==0 && *(player->login) == 1 && *(player->in_match) == 0){
+            char * finding = "finding...";
+            enqueue(player,server->player_queue);
+            printf("enqueue ok\n");
+            while(player->enemy == NULL){
+                sendResponse(player->socket_id,finding,strlen(finding),0);
+                sleep(1);
+            }
+            char * findmessage = "match_found";
+            sendResponse(player->socket_id,findmessage,strlen(findmessage),0);
+            *(player->in_match) = 1;
+        }
+
+        if(strcmp(buffer,"shoot")==0 && *(player->in_match) == 1 && *(player->login) == 1 && player->enemy != NULL){
+            char * damagemessage = "damage";
+            sendResponse(player->enemy->socket_id,damagemessage,strlen(damagemessage),0);
+        }
+
+        if(strcmp(buffer,"logout")==0 && *(player->login) == 1 && *(player->in_match) == 0){
+            *(player->login) = 0;
+            char * logoutmessage = "logout";
+            sendResponse(player->socket_id,logoutmessage,strlen(logoutmessage),0);
+        }
+
+        if(strcmp(buffer,"lose") == 0 && *(player->login) == 1 && *(player->in_match) == 1 && player->enemy != NULL){
+            char * winid = "win";
+            sendResponse(player->enemy->socket_id,winid,strlen(winid),0);
+        }
+        if(strcmp(buffer,"endmatch") == 0 && *(player->login) == 1 && *(player->in_match) == 1 && player->enemy != NULL){
+            *(player->in_match) = 0;
+            player->enemy = NULL;
+        }
+    }
+    free(player);
+}
+
+void* listen_thread(void* args){
+    server_t * serverMain = (server_t *)args;
+    int * server_fd = serverMain->server_socket->server_fd;
+    struct sockaddr_in * address = serverMain->server_socket->address;
+    int * addrlen = serverMain->server_socket->addrlen;
+    while(1){
+
+        printf("listening...\n");
+        if( listen(*server_fd, 3) < 0){
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+
+        int in_match = 0;
+        int login = 0;
+        player_t * temp_player = (player_t *)malloc(sizeof(player_t));
+        temp_player->enemy = NULL;
+        temp_player->in_match = &in_match;
+        temp_player->login = &login;
+
+        printf("accepting...\n");
+        if((temp_player->socket_id = accept(*server_fd,(struct sockaddr *) address, (socklen_t*)addrlen)) < 0){
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        printf("got player socket id %d\n",temp_player->socket_id);
+
+        player_args * thread_args = (player_args*)malloc(sizeof(player_args));
+        thread_args->player = temp_player;
+        thread_args->server = serverMain;
+
+        printf("making player thread...\n");
+        pthread_t p_thread;
+
+        int iret = pthread_create(&p_thread,NULL,player_handler,(void*)thread_args);
+        if(iret){
+            perror("p_thread");
+            exit(EXIT_FAILURE);
+        }
+        printf("player thread made!\n");
+    }
+}
+
+
+int main(){
+    fflush(stdout);
+    socket_t * server_socket = (socket_t *)malloc(sizeof(socket_t));
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    server_socket->server_fd = &server_fd;
+    server_socket->new_socket = &new_socket;
+    server_socket->address = &address;
+    server_socket->opt = &opt;
+    server_socket->addrlen = &addrlen;
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_t * serverMain = (server_t *)malloc(sizeof(server_t));
+    serverMain->server_socket = server_socket;
+    serverMain->player_queue = init_queue();
+    serverMain->akun = fopen(FILE_AKUN,"a+");
+
+    pthread_t listen_pt;
+    int iret = pthread_create(&listen_pt,NULL,listen_thread,(void*)serverMain);
+    if(iret){
+        perror("listen thread");
+        exit(EXIT_FAILURE);
+    }
+    printf("listen thread on\n");
+
+    pthread_t match_pt;
+    int iret1 = pthread_create(&match_pt,NULL,match,(void*)serverMain);
+    if(iret1){
+        perror("match thread");
+        exit(EXIT_FAILURE);
+    }
+    printf("match thread on\n");
+
+    pthread_join(listen_pt,NULL);
+    pthread_join(match_pt,NULL);
+
+    return 0;
+
+}
